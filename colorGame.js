4999d77cbf9af95ef8ad158794754a133d764521
Andrew <andrewrosca7@gmail.com>
2019-01-05 22:17:50 -0800

    adding finished project

---
+++ colorGame.js
@@ -0,0 +1,133 @@
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+resetButton.addEventListener("click", function () {
+    // generate all new colors
+    colors = generateRandomColors(numSquares);
+    // pick a new random color from array
+    pickedColor = pickColor();
+    // change colorDisplay to march picked color
+    colorDisplay.textContent = pickedColor;
+    this.textContent = "New Colors"
+    messageDisplay.textContent = "";
+    // change colors of squares 
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i]; 
+    }
+    h1.style.backgroundColor = "steelblue";
+});
+
+
+// Easy Button Clicked
+easyBtn.addEventListener("click", function() {
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numSquares = 3; 
+    colors = generateRandomColors(numSquares); 
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor; 
+    for(var i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+        squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = "none"; 
+        }
+    }
+}); 
+
+
+// Hard Button Clicked
+hardBtn.addEventListener("click", function() {
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected"); 
+    numSquares = 6; 
+    colors = generateRandomColors(numSquares); 
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor; 
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i];
+        squares[i].style.display = "block"; 
+    }
+});
+
+
+
+colorDisplay.textContent = pickedColor;
+for (var i = 0; i < squares.length; i++) {
+    // add initial colors to squares
+    squares[i].style.backgroundColor = colors[i];
+    // add click listeners to squares
+    squares[i].addEventListener("click", function () {
+        // grab color of clicked square
+        var clickedColor = this.style.backgroundColor
+        
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again?"
+            changeColors(clickedColor);
+            h1.style.background = clickedColor;
+        }
+        else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
+    });
+}
+
+
+
+
+// change color to all squares when correct square picked
+function changeColors(color) {
+    // loop through all squares
+    for (var i = 0; i < squares.length; i++) {
+        // change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+
+
+// generate random numbers 
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+
+
+
+function generateRandomColors(num) {
+    // make an array
+    var array = []
+        // add num random colors to array  
+    for (var i = 0; i < num; i++) {
+        // get random color and push into array
+        array.push(randomColor());
+    }
+    // return that array
+    return array;
+}
+
+
+
+
+function randomColor() {
+    // pick a "red" from 0 -225
+    var red = Math.floor(Math.random() * 256);
+    // pick a "green" from 0 -255
+    var green = Math.floor(Math.random() * 256)
+        // pick a "blue" from 0 -255
+    var blue = Math.floor(Math.random() * 256)
+    "rgb(r, g, b)"
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
+}
